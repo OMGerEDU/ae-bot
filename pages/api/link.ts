d6351ac1bbf6fc93aeea913a77733c9af9967c49
OmerDahan-at-637423475228 <omger@edu.aac.ac.il>
2025-05-10 15:59:08 +0300

    bruh here's an update

--- pages/api/link.ts
+++ pages/api/link.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { generateLink } from '../../../lib/aliexpress';
+import { generateLink } from '../../lib/aliexpress';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "bruh here's an update"
+git push origin main
